538ecf00ef77235f57b580224245a01f99bb2265
Leo <l.percivati@gmail.com>
2019-11-14 20:35:00 -0300

    Tabla con resultados

--- App.js
+++ App.js
@@ -1,34 +1,56 @@
 import * as React from 'react';
-import { Button, Image, View } from 'react-native';
+import { Button, Image, View, Alert, StyleSheet, Text } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import Constants from 'expo-constants';
 import * as Permissions from 'expo-permissions';
-
+import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
 
 export default class ImagePickerExample extends React.Component {
   state = {
     image: null,
+    mostrarResultados: false,
+    filas: [],
+    tableHead: ["Alimento", "Porcentaje"]
   };
 
   render() {
-    let { image } = this.state;
 
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Button style={{ marginBottom:100 }}
-          title="Pick an image from camera roll"
-          onPress={this._pickImage}
-        />
-
-        <Button style={divStyle}
-          title="Open camera"
-          onPress={this._takePhoto}
-        />
-
-        {image &&
-          <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+        { this.renderizar() }
       </View>
     );
+     
+  }
+
+  renderizar(){
+    let { image } = this.state;
+
+    if(this.state.mostrarResultados){
+      return(
+      <View style={styles.container}>
+        <Text>Reconocimiento de imagenes</Text>
+        
+        <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
+          <Row data={this.state.tableHead} style={styles.head} textStyle={styles.text}/>
+          <Rows data={this.state.filas} textStyle={styles.text}/>
+        </Table>
+      </View>
+      )
+
+    }else{
+      return (
+      <View >
+      
+        <Button style={{ marginBottom:100 }} title="Pick an image from camera roll" onPress={this._pickImage} />
+
+        <Button style={divStyle} title="Open camera" onPress={this._takePhoto} />
+
+        {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      
+      </View>
+      )
+    }
   }
 
   componentDidMount() {
@@ -46,34 +68,31 @@ export default class ImagePickerExample extends React.Component {
     let result = await ImagePicker.launchImageLibraryAsync(options);
 
     if (!result.cancelled) {
-      this.setState({ image: result.uri });
       this.identifyImage(result.base64);
     }
-    
   };
 
   _takePhoto = async () => {
     let result = await ImagePicker.launchCameraAsync(options);
   
     if (!result.cancelled) {
-      this.setState({ image: result.uri });
       this.identifyImage(result.base64);
     }
   };
 
   identifyImage(imageData){
-    const Clarifai = require('clarifai');
-    const app = new Clarifai.App({apiKey: '5234ecbf19af4f7ea6bf659ebe861907'});
     app.models.predict(Clarifai.FOOD_MODEL, {base64: imageData})
-        .then((response) =>  this.parseResponse(response)
-        .catch((err) => alert(err))
-    );
+        .then((response) => {
+          this.parseResponse(response)
+        })
+        .catch((err) => alert(err));
 }
 
   parseResponse(response){
-    response.outputs[0].data.concepts.map((item, key) =>
-    console.log(item.name+" : "+item.value)
-    )
+    this.setState({
+      mostrarResultados: true,
+      filas: response.outputs[0].data.concepts.map(item => [item.name, item.value])
+    })
   }
   
 }
@@ -89,3 +108,12 @@ let options = {
 const divStyle = {
   margin: 100
 };
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
+  head: { height: 40, backgroundColor: '#f1f8ff' },
+  text: { margin: 6 }
+});
+
+const Clarifai = require('clarifai');
+const app = new Clarifai.App({apiKey: '5234ecbf19af4f7ea6bf659ebe861907'});
