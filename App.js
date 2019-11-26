156da0c0377cc98cfd29d777ddabd969100aae18
Leo <l.percivati@gmail.com>
2019-11-26 20:09:41 -0300

    Agregado de segundo modelo y mejoras

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import * as FileSystem from 'expo-file-system';
 
 export default class ImagePickerExample extends React.Component {
   state = {
+    lastPhoto: null,
     image: null,
     mostrarResultados: false,
     filas: [],
@@ -43,6 +44,12 @@ export default class ImagePickerExample extends React.Component {
           <Image source={{ uri: this.state.image }} style={styles.imagen} />
         </View>
         <View style={{ flex:3}}>
+          { this.state.animating &&
+            <View style={ styles.loading }>
+              <ActivityIndicator style={{opacity: this.state.animating ? 1.0 : 0.0}} animating={true} color='#bc2b78' size="large"/>
+
+            </View>
+          }
             <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
               <Row data={this.state.tableHead} style={styles.head} textStyle={styles.text}/>
               <ScrollView>
@@ -53,7 +60,10 @@ export default class ImagePickerExample extends React.Component {
         </View>
         <View style={styles.boxVolver}>
           <Icon raised size={30} name='arrow-left' type='font-awesome' color='#630090' onPress={this.irAInicio} />
-          <Icon raised size={30} name='arrow-down' type='font-awesome' color='#630090' onPress={this.saveFile}/>
+          <Icon raised size={30} name='download' type='font-awesome' color='#630090' onPress={this.saveFile}/>
+          {this.state.lastPhoto &&
+          <Icon raised size={30} name='retweet' type='font-awesome' color='#630090' onPress={this.analizarSegundoModelo}/>
+          }
         </View>
       </View>
       )
@@ -83,6 +93,13 @@ export default class ImagePickerExample extends React.Component {
         await FileSystem.writeAsStringAsync(fileUri, contenFile, { encoding: FileSystem.EncodingType.UTF8 });
         const asset = await MediaLibrary.createAssetAsync(fileUri)
         await MediaLibrary.createAlbumAsync("Download", asset, false)
+        Alert.alert(
+          'Download Successful!',
+          'You can find your file in Downloads.',
+          [
+            {text: 'Ok'}
+          ]
+        );
     }
   }
 
@@ -110,14 +127,14 @@ export default class ImagePickerExample extends React.Component {
     let result = await ImagePicker.launchImageLibraryAsync(options);
 
     if (!result.cancelled) {
-      this.identificarImagen(result.base64, result.uri);
+      this.identificarImagen(result.base64, result.uri, false);
     }
   };
 
   abrirCamara = async () => {
     let result = await ImagePicker.launchCameraAsync(options);
     if (!result.cancelled) {
-      this.identificarImagen(result.base64, result.uri);
+      this.identificarImagen(result.base64, result.uri, false);
     }
   };
 
@@ -127,24 +144,46 @@ export default class ImagePickerExample extends React.Component {
     })
   }
 
-  identificarImagen(imageDataBase64, imageUri){
+  analizarSegundoModelo = ()=>{
+    this.identificarImagen(null, null, true);
+  }
+
+  identificarImagen(imageDataBase64, imageUri, segundaOpcion){
     this.setState({
       animating: true
     });
 
-    app.models.predict(Clarifai.FOOD_MODEL, {base64: imageDataBase64})
-        .then((response) => {
-          this.setState({
-            animating: false,
-            image: imageUri,
-            mostrarResultados: true,
-            filas: response.outputs[0].data.concepts
-              .filter(item => item.value > 0.6)
-              .map(item => [item.name, (item.value * 100).toFixed(2) + "%"])
-          })
+    if(!segundaOpcion){
+      this.setState({
+        lastPhoto: imageDataBase64
+      });
+
+      app.models.predict(Clarifai.FOOD_MODEL, {base64: imageDataBase64})
+      .then((response) => {
+        this.setState({
+          animating: false,
+          image: imageUri,
+          mostrarResultados: true,
+          filas: response.outputs[0].data.concepts
+            .filter(item => item.value > 0.6)
+            .map(item => [item.name, (item.value * 100).toFixed(2) + "%"])
         })
-        .catch((err) => alert(err));
-        
+      })
+      .catch((err) => alert(err));
+    }else{
+      app.models.predict(Clarifai.GENERAL_MODEL, {base64: this.state.lastPhoto})
+      .then((response) => {
+        this.setState({
+          lastPhoto: null,
+          animating: false,
+          mostrarResultados: true,
+          filas: response.outputs[0].data.concepts
+            .filter(item => item.value > 0.6)
+            .map(item => [item.name, (item.value * 100).toFixed(2) + "%"])
+        })
+      })
+      .catch((err) => alert(err));
+    }
   }  
 }
 
@@ -177,10 +216,11 @@ const styles = StyleSheet.create({
     textShadowRadius:15
   },
   boxVolver:{
+    flexDirection: "row",
     flex:1, 
     marginTop: 10, 
-    justifyContent: "flex-end",  
-    alignItems:"center" 
+    justifyContent: "center",  
+    alignItems:"flex-end" 
   },
   imagen:{ 
     width: 200, 
@@ -194,6 +234,15 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start", 
     alignItems:"center", 
     marginBottom:10 
+  },
+  loading: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
 
